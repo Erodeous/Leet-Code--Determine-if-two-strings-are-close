5eb3304998bc8c8690e06636ab5541b944191dd3
Andrew Gamsaragan <119918876+Erodeous@users.noreply.github.com>
2022-12-05 22:47:24 -0800

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        print(set(word1))
+
+        if len(word1) != len(word2) or set(word1) != set(word2): return False
+        else:
+            if sorted([word1.count(i) for i in set(word1)])== sorted([word2.count(i) for i in set(word2)]):
+                return True
+        return False
